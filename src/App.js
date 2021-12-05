4e669ef25a1bf558cf7a9cc8833e4da3918e0c85
developer@trashcash.ph <sacdalanryanchristian6404@gmail.com>
2021-12-05 22:09:03 +0800

    fix app.js, render catched errors

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
-import BlogList from "./Components/BlogList";
 import Create from "./Components/Create";
 import Homepage from "./Homepage";
 import "./App.css";
 
 function App() {
-  const [blogs, setBlogs] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/Blogs")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        console.log(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
   return (
     <Router>
       <Switch>
         <div className="App">
           <Navbar />
           <Route exact path="/">
-            <Homepage blogs={blogs} />
+            <Homepage />
           </Route>
           <Route path="/create">
             <Create />
--- src/Homepage.js
+++ src/Homepage.js
@@ -1,10 +1,47 @@
+import React, { useState, useEffect } from "react";
 import BlogList from "./Components/BlogList";
 
-const Homepage = ({ blogs }) => {
+const Homepage = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [loadPending, setLoadPending] = useState(true);
+  const [errorExist, setErrorExist] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("http://localhost:8000/Blogs")
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Oops! Could not fetch the data :( ");
+          } else {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          setBlogs(data);
+          setLoadPending(false);
+        })
+        .catch((err) => {
+          setLoadPending(false);
+          setErrorExist(err.message);
+          console.log(err.message);
+        });
+    }, 0.5);
+  }, []);
+
   return (
     <div>
       <h1>Homepage</h1>
-      <BlogList blogs={blogs} />
+      {loadPending && (
+        <div>
+          <h2>Loading...</h2>
+        </div>
+      )}
+      {BlogList && <BlogList blogs={blogs} />}
+      {errorExist && (
+        <div>
+          <h2>{errorExist}</h2>
+        </div>
+      )}
     </div>
   );
 };
